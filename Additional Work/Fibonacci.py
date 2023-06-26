7fd1df2853a6207f7e4ab2a9fca5c960a4f9c3f1
dvihaan <dvihaan1@gmail.com>
2023-06-26 10:47:05 -0400

    added performance counter to Fibonacci

--- Additional Work/Fibonacci.py
+++ Additional Work/Fibonacci.py
@@ -1,20 +1,57 @@
-def fibonacci(n, outStr):
+import time
+
+def fibonacci(n):
     if n == 0 or n == 1:
-        return 1, outStr + '1,'
+        return 1
     else:
-        result_1,outStr_1 = fibonacci(n-1,outStr)
-        result_2,outStr_2 = fibonacci(n-2,outStr)
-        result = result_1 + result_2
-        outStr = outStr + str(result_2) + ','+ str(result_1)+','
-        return result,outStr
+        result = fibonacci(n-1) + fibonacci(n-2)
+        return result
+    
+def fibWdict(lvl,d):
+    if lvl not in d:
+        res = fibWdict(lvl-1,d)[0] + fibWdict(lvl-2,d)[0]
+        d[lvl] = res
+        return res,d
+    else:
+        return d[lvl],d
 
 if __name__ == "__main__":
     entered = int(input("Enter the number of fibonacci levels: ")) - 1
+    '''
     outStr = "Fib("+str(entered+1)+") = "
     final,outStr = fibonacci(entered,outStr)
     print(outStr+str(final))
+    '''
+
+
+    print("Calculating Fibonacci numbers using pruned recursion with memory...")
+    print("")
+
+    start = time.perf_counter()
+    fibdict = {1:1, 2:2}
+    result, dictionary = fibWdict(entered,fibdict)
+    print(dictionary.values())
+    stop = time.perf_counter()
+    print(str((stop-start)*1000) + " milliseconds elapsed")
+
+    print("")
+
+    print("Calculating Fibonacci numbers using basic recursion...")
+    print("")
+
+    start = time.perf_counter()
+    result = fibonacci(entered)
+    print("Fib("+str(entered)+")="+str(result))
+    stop = time.perf_counter()
+    print(str((stop-start)*1000) + " milliseconds elapsed")
+
+
+
+
+
+
 
 
 '''
-1,1,2,3,5,8,13,21,34,55
+fib(10) = 1,1,2,3,5,8,13,21,34,55
 '''
