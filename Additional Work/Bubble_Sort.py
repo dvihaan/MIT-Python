77b245919098eded2c43dd07ac5d31cfe08e64a4
dvihaan <dvihaan1@gmail.com>
2023-07-09 16:44:09 -0400

    Bubble Sort in additional work folder

---
+++ Additional Work/Bubble_Sort.py
@@ -0,0 +1,16 @@
+def bubbleSort(L):
+    swap = True
+    while swap:
+        swap = False
+        for i in range(1, len(L), 1):
+            if L[i-1] > L[i]:
+                temp = L[i-1]
+                L[i-1] = L[i]
+                L[i] = temp
+                swap = True
+
+if __name__ == '__main__':
+    aList = [9,3,7,8,5,6,4,2,1]
+    print(aList)
+    bubbleSort(aList)
+    print(aList)
